fb607d587cc9653bbdf061bd760cdc381fa9d410
Yuan <yuan.ncse.tw@gmail.com>
2025-03-24 11:57:58 +0800

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const https = require("https");
 const express = require("express");
 const app = express();
 const PORT = 3001;
-const TOKEN = "yWT9DrE6vngujYX5Du/fqxIo3yzubvOwOJf68p5gH9Vk7SUY1AhXMank03hxD009qPaLgApo8+iyEXZuexkqWN6ViWGTZ/jBJACzLdJ347vWpvS4XTUbCa082NPrls1icBftFUW4eta7TavSIr+KbAdB04t89/1O/w1cDnyilFU=";
+const TOKEN = "";
 const {
   GoogleGenerativeAI,
   HarmCategory,
